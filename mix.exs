3d78ae67d227a1a3cb178cfa9006b2568754ac72
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-02-02 15:46:27 -0500

    chore: add credo and uberauth

--- mix.exs
+++ mix.exs
@@ -42,7 +42,9 @@ defmodule Sibt.MixProject do
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:ueberauth, "~> 0.5"},
+      {:credo, "~> 1.0"}
     ]
   end
 
