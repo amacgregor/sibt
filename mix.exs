fd3ce16e36240be07afac5c0c9a0a8a5355bf326
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-02-03 10:13:58 -0500

    feat(versioning): move application version to variable
    Move versioning to variable for git_ops control

--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule Sibt.MixProject do
   use Mix.Project
 
+  @version "0.2.0"
+
   def project do
     [
       app: :sibt,
-      version: "0.2.0",
+      version: @version,
       elixir: "~> 1.5",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
