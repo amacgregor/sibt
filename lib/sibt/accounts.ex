d8b27d7400e562cf39511c088027c8c4b28cb708
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-07-28 18:39:06 -0400

    feat: add accounts context with matching tests

---
+++ lib/sibt/accounts.ex
@@ -0,0 +1,125 @@
+defmodule Sibt.Accounts do
+  @moduledoc """
+  The Accounts context.
+  """
+
+  import Ecto.Query, warn: false
+  alias Sibt.Repo
+  alias Sibt.User
+
+  require Logger
+
+
+  @spec prepare_authenticated_user_changeset(Ueberauth.Auth.t()) :: {:ok, Ecto.Changeset.t()}
+  def prepare_authenticated_user_changeset(%Ueberauth.Auth{credentials: credentials, info: info, provider: :github} = auth) do
+    names = String.split(info.name)
+    user_params = %{
+      token: credentials.token,
+      first_name: Enum.at(names, 0),
+      last_name: Enum.at(names, 1),
+      email: info.email,
+      provider: Atom.to_string(auth.provider)
+    }
+
+    {:ok, User.changeset(%User{}, user_params)}
+  end
+
+  def prepare_authenticated_user_changeset(%Ueberauth.Auth{credentials: credentials, info: info, provider: :facebook} = auth) do
+    user_params = %{
+      token: credentials.token,
+      first_name: info.first_name,
+      last_name: info.last_name,
+      email: info.email,
+      provider: Atom.to_string(auth.provider)
+    }
+
+    {:ok, User.changeset(%User{}, user_params)}
+  end
+
+  @doc """
+  Returns the list of users.
+
+  ## Examples
+
+      iex> list_users()
+      [%Users{}, ...]
+
+  """
+  def list_users do
+    raise "TODO"
+  end
+
+  @doc """
+  Gets a single users.
+
+  Raises if the Users does not exist.
+
+  ## Examples
+
+      iex> get_users!(123)
+      %Users{}
+
+  """
+  def get_users!(id), do: raise "TODO"
+
+  @doc """
+  Creates a users.
+
+  ## Examples
+
+      iex> create_users(%{field: value})
+      {:ok, %Users{}}
+
+      iex> create_users(%{field: bad_value})
+      {:error, ...}
+
+  """
+  def create_users(attrs \\ %{}) do
+    raise "TODO"
+  end
+
+  @doc """
+  Updates a users.
+
+  ## Examples
+
+      iex> update_users(users, %{field: new_value})
+      {:ok, %Users{}}
+
+      iex> update_users(users, %{field: bad_value})
+      {:error, ...}
+
+  """
+  def update_users(%User{} = users, attrs) do
+    raise "TODO"
+  end
+
+  @doc """
+  Deletes a Users.
+
+  ## Examples
+
+      iex> delete_users(users)
+      {:ok, %Users{}}
+
+      iex> delete_users(users)
+      {:error, ...}
+
+  """
+  def delete_users(%User{} = users) do
+    raise "TODO"
+  end
+
+  @doc """
+  Returns a data structure for tracking users changes.
+
+  ## Examples
+
+      iex> change_users(users)
+      %Todo{...}
+
+  """
+  def change_users(%User{} = users) do
+    raise "TODO"
+  end
+end
---
+++ test/sibt/accounts_test.exs
@@ -0,0 +1,85 @@
+defmodule Sibt.AccountsTest do
+  use Sibt.DataCase
+
+  alias Sibt.Accounts
+
+  describe "users" do
+    alias Sibt.User
+
+    @valid_attrs %{}
+    @update_attrs %{}
+    @invalid_attrs %{}
+
+    @ueberauth_github_auth %Ueberauth.Auth{
+      credentials: %{token: "asdf12334567890"},
+      info: %{email: "tony@thetiger.com", name: "Tony Stark"},
+      provider: :github
+    }
+
+    def github_user_fixture() do
+      user_params = %{
+        email: "tony@thetiger.com",
+        first_name: "Tony",
+        last_name: "Stark",
+        provider: "github",
+        token: "asdf12334567890"
+      }
+
+      {:ok, User.changeset(%User{}, user_params)}
+
+    end
+
+    def users_fixture(attrs \\ %{}) do
+      {:ok, users} =
+        attrs
+        |> Enum.into(@valid_attrs)
+        |> Accounts.create_users()
+
+      users
+    end
+
+    test "prepare_authenticated_user_changeset/1 returns a valid changeset" do
+      assert Accounts.prepare_authenticated_user_changeset(@ueberauth_github_auth) == github_user_fixture()
+    end
+
+    test "list_users/0 returns all users" do
+      users = users_fixture()
+      assert Accounts.list_users() == [users]
+    end
+
+    test "get_users!/1 returns the users with given id" do
+      users = users_fixture()
+      assert Accounts.get_users!(users.id) == users
+    end
+
+    test "create_users/1 with valid data creates a users" do
+      assert {:ok, %User{} = users} = Accounts.create_users(@valid_attrs)
+    end
+
+    test "create_users/1 with invalid data returns error changeset" do
+      assert {:error, %Ecto.Changeset{}} = Accounts.create_users(@invalid_attrs)
+    end
+
+    test "update_users/2 with valid data updates the users" do
+      users = users_fixture()
+      assert {:ok, %User{} = users} = Accounts.update_users(users, @update_attrs)
+    end
+
+    test "update_users/2 with invalid data returns error changeset" do
+      users = users_fixture()
+      assert {:error, %Ecto.Changeset{}} = Accounts.update_users(users, @invalid_attrs)
+      assert users == Accounts.get_users!(users.id)
+    end
+
+    test "delete_users/1 deletes the users" do
+      users = users_fixture()
+      assert {:ok, %User{}} = Accounts.delete_users(users)
+      assert_raise Ecto.NoResultsError, fn -> Accounts.get_users!(users.id) end
+    end
+
+    test "change_users/1 returns a users changeset" do
+      users = users_fixture()
+      assert %Ecto.Changeset{} = Accounts.change_users(users)
+    end
+  end
+end
