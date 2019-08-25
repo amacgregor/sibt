fdcc5666cca16bd8bcbf493181dc42b112eacc26
Allan MacGregor <amacgregor@allanmacgregor.com>
2019-08-25 10:46:55 -0400

    format: update mix format

--- mix.exs
+++ mix.exs
@@ -11,7 +11,12 @@ defmodule Sibt.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
